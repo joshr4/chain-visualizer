faaf41de84b662d01121be156e5d109bbe3a8a90
Josh Remaley <joshremaley@gmail.com>
2019-05-28 22:24:11 -0500

    test change

--- src/App.js
+++ src/App.js
@@ -11,14 +11,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p className="title">
+            Babaganoush!
+          </p>
         </header>
       </div>
     );
